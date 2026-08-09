f388751a947f2194487e863977ce450e5dfbda1a
mauthanh04 <mauthanh00004@gmail.com>
2026-08-09 22:12:10 +0700

    first commit3

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,7 @@
 import app from '../server/app';
 
-export default app;
+export default function handler(req: any, res: any) {
+	// Export a function wrapper so Vercel's Node.js Serverless Functions
+	// can invoke the Express app correctly.
+	return app(req, res);
+}
